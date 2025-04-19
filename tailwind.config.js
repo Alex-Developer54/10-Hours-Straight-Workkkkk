96686923be96dc4b5b4297994e164c5a5ba4b2ad
Alex_Developer54 <ha40271947@gmail.com>
2025-04-19 17:14:15 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./*.html", "./**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        instrument: ['"Instrument Sans"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
